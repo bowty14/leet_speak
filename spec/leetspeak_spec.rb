79bc1c1172f8b007189aeaefc9b4340e2a6fe99d
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-04 14:28:53 -0700

    fail test

--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -8,4 +8,7 @@ describe('#leet_speak') do
   it('replaces every letter "o" with "0"') do
     expect(leet_speak("look")).to(eq("l00k"))
   end
+  it('replaces both "e" & "o" in a sentence with "3" & "0"') do
+    expect(leet_speak("these are words")).to(eq("th3s3 ar3 w0rds"))
+  end
 end
