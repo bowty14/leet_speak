1d7b57f68bcd8612d26a36f5ad559efe80497c9c
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-04 14:01:48 -0700

    run test on 2nd spec, expect to fail. test failed

--- spec/leetspeak_spec.rb
+++ spec/leetspeak_spec.rb
@@ -5,4 +5,7 @@ describe('#leetspeak') do
   it('returns the letter "e" in a sentence with the number 3') do
     expect(leet_speak("hello")).to(eq("h3llo"))
   end
+  it('replaces every letter "o" with "0"') do
+    expect(leet_speak("look")).to(eq("l00k"))
+  end
 end
