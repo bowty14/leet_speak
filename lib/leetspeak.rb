08c3a8befc809956c5d9e48d6fde2a91ae16112c
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-04 14:24:55 -0700

    test fails as expected

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -5,8 +5,8 @@ def leet_speak(sentence)
   if sentence.include?("e") 
     new_sentence = sentence.gsub("e", "3")
   end 
-  if sentence.include?("o")
-    new_sentence = sentence.gsub("o", "0")
-  end
+  # if sentence.include?("o")
+  #   new_sentence = sentence.gsub("o", "0")
+  # end
   new_sentence
 end
