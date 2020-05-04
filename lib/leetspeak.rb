0a67c92531e86f20832b6ac8058f4b76c83ab30b
Zinfira Safargalieva <zsafargalieva@gmail.com>
2020-05-04 14:26:01 -0700

    add text to pass test #2, test passes.

--- lib/leetspeak.rb
+++ lib/leetspeak.rb
@@ -3,10 +3,9 @@ require('pry')
 
 def leet_speak(sentence) 
   if sentence.include?("e") 
-    new_sentence = sentence.gsub("e", "3")
-  end 
-  # if sentence.include?("o")
-  #   new_sentence = sentence.gsub("o", "0")
-  # end
+    new_sentence = sentence.gsub("e", "3") 
+  elsif sentence.include?("o")
+    new_sentence = sentence.gsub("o", "0")
+  end
   new_sentence
 end
